78171172b3c7e0cbbf5dbbbffa25006ea55c2cb6
baosonnguyendang <baosonnguyendang@gmail.com>
2021-10-14 10:58:18 +0700

    day moi la lan cuoi

--- Angular/src/app/employee/employee.component.ts
+++ Angular/src/app/employee/employee.component.ts
@@ -137,7 +137,7 @@ export class EmployeeComponent implements OnInit {
       }
       else {
         console.log('cassiopeia')
-        console.log('caribeean')
+        console.log('caribean')
         console.log('casablanca')
       }
     });
